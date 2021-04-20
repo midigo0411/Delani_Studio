d81b5cfd51ca9e282221c02a5714fc3b04f566c8
midigo0411 <midigo041@gmail.com>
2021-04-20 03:02:52 +0300

    adding the slide show for development

--- js/scripts.js
+++ js/scripts.js
@@ -8,6 +8,18 @@ $(document).ready(function(){
         $(this).slideUp();
         $(".drop1").show(500);
 });
+    $(".drop1").click(function(){
+        $("#projectimage").slideDown();
+        $(".drop1").hide();
+});
+    $("#projectimage0").click(function(){
+        $(this).slideUp();
+        $(".drop2").show(500);
+});
+    $(".drop2").click(function(){
+    $("#projectimage0").slideDown();
+    $(".drop2").hide();
+ });
 
 });
     
